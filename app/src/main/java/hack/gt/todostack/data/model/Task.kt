e14ab9ce09f259cad2343f4cf6f1002edaf30021
Ryan Miles <ryan.miles@bluefletch.com>
2020-10-17 12:44:34 -0400

    fix task

--- app/src/main/java/hack/gt/todostack/data/model/Task.kt
+++ app/src/main/java/hack/gt/todostack/data/model/Task.kt
@@ -1,2 +1,12 @@
 package hack.gt.todostack.data.model
 
+import java.time.Duration
+import java.util.*
+
+data class Task(
+    var title: String,
+    var deadline: Date,
+    var estimatedTime: Duration,
+    var difficulty: Int = 0,
+    var importance: Int = 0
+)
