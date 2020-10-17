214ff6fa56a8e7fc3aa0fb4879ddb45184f387b0
Ryan Miles <ryan.miles@bluefletch.com>
2020-10-17 12:50:39 -0400

    fix task

--- app/src/main/java/hack/gt/todostack/data/TaskRepository.kt
+++ app/src/main/java/hack/gt/todostack/data/TaskRepository.kt
@@ -1,2 +1,16 @@
 package hack.gt.todostack.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import hack.gt.todostack.data.model.Task
+
+object TaskRepository {
+
+    private val tasks: List<Task> = emptyList()
+    private val _tasksLiveData = MutableLiveData<List<Task>>().apply {
+        value = tasks
+    }
+    private val tasksLiveData: LiveData<List<Task>> = _tasksLiveData
+
+    fun getTasks() = _tasksLiveData
+}
